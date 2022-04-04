f6ffeda814f06c095b4e09451398e8c4baacc00d
Frankz <leninalarcon7@gmail.com>
2022-04-04 14:43:43 -0500

    bug solved

--- src/pages/PacientList.jsx
+++ src/pages/PacientList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import useCustomersValues from '../hooks/useCustomersValues';
 import CustomerCard from '../components/CustomerCard';
 import Searcher from '../components/Searcher';
@@ -36,8 +37,24 @@ function PacientList() {
         {' '}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-      <Searcher value={inputData} setValue={setInputData} placeholder="Busca a un paciente por su propietario" />
       {
+        customers.length === 0 ? (
+          <>
+            <h1 className="text-center text-xl pt-5 md:text-2xl lg:text-3xl lg:mb-6 font-black">
+              No tienes registros de
+              {' '}
+              <span className="text-indigo-600">Pacientes Pendientes</span>
+            </h1>
+            <p className="text-center text-xl my-3">
+              Para agregar un registro da clic en:
+              {' '}
+              <span className="text-indigo-600 font-bold"><Link to="/new-patient">Nuevo Paciente</Link></span>
+            </p>
+          </>
+        ) : (
+          <>
+            <Searcher value={inputData} setValue={setInputData} placeholder="Busca a un paciente por su propietario" />
+            {
         filteredCustomers.length === 0 ? (
           <Alerta type="alert">
             No existen coincidencias con
@@ -48,6 +65,9 @@ function PacientList() {
           : filteredCustomers.map((customer) => (
             <CustomerCard key={customer.id} customer={customer}>{customer.petName}</CustomerCard>
           ))
+          }
+          </>
+        )
       }
     </>
   );
